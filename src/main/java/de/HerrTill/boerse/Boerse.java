7d62fe91dc4485a8c052cd635cb5948301629065
herrtill <tenorhorn7@gmail.com>
2015-11-04 14:17:16 +0100

    NEWS!

--- src/main/java/de/HerrTill/boerse/Boerse.java
+++ src/main/java/de/HerrTill/boerse/Boerse.java
@@ -1,4 +1,4 @@
-package de.HerrTill.minecraft.boerse;
+package de.HerrTill.boerse;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.*;
@@ -10,25 +10,24 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
  * Created by herrtill on 04.11.15.
  */
 
-@Mod(modid = "boerse", name = "Börse", version = "1.0")
+@Mod(modid = "boerse", name = "Boerse", version = "1.0")
 public class Boerse {
 
     @Instance
     public static Boerse instance = new Boerse();
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent e) {
-
+    public void preInit (FMLPreInitializationEvent e){
+        System.out.println("preInit-Methode geladen!");
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent e) {
-
+    public void init (FMLInitializationEvent e) {
+        System.out.println("init-Methode geladen!");
     }
 
     @EventHandler
-    public void postInit(FMLPostInitializationEvent e) {
-
+    public void postInit (FMLPostInitializationEvent e){
+        System.out.println("postInit-Methode geladen!");
     }
-
 }
